4829877ec0c29be10d3da6716f251825831bdea3
gkalluru77 <girinath.kalluru@gmail.com>
2023-12-12 18:20:26 +0530

    terraform

--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "parameters" {
   default = [
   {  name="ses.username", type="string", value="AKIA4Y7OBINMCHRL2LJ3" },
-  {  name="dev.rds.username", type="String" value="admin1" },
+  {  name="dev.rds.username", type="String", value="admin1" },
   { name = "dev.rds.password", type="SecureString", value="ExpenseApp1234" },
   ]
   }
